c1cf612bc3099924597dd396802fd221b7270fd3
URFUTEAM <european.ass@gmail.com>
2021-11-18 20:42:13 +0500

    done: all

--- src/task_1/index.ts
+++ src/task_1/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 /** Задача 1 - Сущность любой монетки
  * Опишите класс валюты
  * Он должен определять имя(name) валюты, String
@@ -8,9 +9,48 @@
  */
 
 export class Currency{
+   private _name: string;
+   private _value: number;
+   private _currencyType: CurrencyType;
+   private _unit: string
+    type: any;
 
+   get name() {
+      return this._name;
+    }
+  
+    get value() {
+      return this._value;
+    }
+    set value(newValue: number) {
+      this._value = newValue;
+    }
+    get unit() {
+      return this._unit;
+    }
+  
+    get currencyType() {
+      return this._currencyType;
+    }
+
+   constructor(name: keyof typeof CurrencyType, value: number, unit: string) {
+      if (!name || value < 0 || value === undefined || !unit) {
+        throw new Error("Invalid arguments");
+      }
+      this._name = name;
+      this._value = value;
+      this._unit = unit;
+      this._currencyType = CurrencyType[name];
+    }
 }
 
 export enum CurrencyType {
-
+   "Ruble" = "Material",
+   "XRP" = "Crypto",
+   "Etherium" = "Crypto",
+   "Gold" = "Metal",
+   "ru" = "Material",
+   "alpha" = "Crypto",
+   "Dollar" = "Material",
+   
 }
--- src/task_3/index.ts
+++ src/task_3/index.ts
@@ -7,11 +7,47 @@
 import { Currency } from "../task_1";
 
 export class Vault implements ISecureVaultRequisites{
+	
 	public id: number;
 	public store: Set<Currency> = new Set<Currency>()
+	private static counterId = 0; 
+	constructor() {
+		this.id = Vault.counterId++;
+  }
+	public withdraw(currency: Currency) {
+		this.store.forEach((value) => {
+		  if (value.name === currency.name && value.value >= currency.value) {
+			value.value -= currency.value;
+		  } else {
+			throw new Error("Invalid");
+		  }
+		});
+	  }
+
+	public deposit(currency: Currency){
+		let thereIsCurrency = false;
+
+		this.store.forEach((value)=>{
+			if(value.name === currency.name){
+				thereIsCurrency = true;
+				value.value+=currency.value;
+			}
+		})
+
+		if(!thereIsCurrency){
+			this.store.add(currency);
+		}
+	}
+
+	public transfer (currency: Currency, vault: Vault){
+		this.withdraw(currency);
+		vault.deposit(currency);
+	}
 }
 
 
 export interface ISecureVaultRequisites{
-	id: number
+	id: number;
+	withdraw: (currency: Currency) => void;
+	deposit: (currency: Currency) => void;
 }
